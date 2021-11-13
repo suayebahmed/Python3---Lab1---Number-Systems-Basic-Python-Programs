2a7425f6f945d142ceca35ea9aa776166cc15ad4
Suayeb Ahmed <63619068+suayebahmed@users.noreply.github.com>
2021-11-13 15:27:55 -0600

    Add files via upload

---
+++ FavoriteQuotes.py
@@ -0,0 +1,13 @@
+# THREE FAV QUOTES OF MINE
+
+#Quote 1
+print("\"We must not allow other people’s limited perceptions to define us.\"")
+print("- Virginia Satir\n")
+
+#Quote 2
+print("\"Do what you can, with what you have, where you are.\"")
+print("- Theodore Roosevelt\n")
+
+#Quote 3
+print("\"Be yourself; everyone else is already taken.\"")
+print("- Oscar Wilde\n")
